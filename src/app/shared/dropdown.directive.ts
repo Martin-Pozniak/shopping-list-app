f0a31d55b7e4c5e364f7e2dbc98a1585110b52d9
martin-pozniak <mpozniak@mmm.com>
2019-07-30 13:18:16 -0500

    Changed dropdown directive behavior to close the opened dropdown from anywhere on the page.

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,13 +1,13 @@
-import {Directive, HostListener, HostBinding} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
-  selector: '[appDropdown]',
+  selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostBinding('class.open') menuOpened: boolean = false;
-
-  @HostListener('click') toggleOpen() {
-    this.menuOpened = !this.menuOpened;
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
-
+  constructor(private elRef: ElementRef) {}
 }
+// This code chunk was taken from course to enable closing of the menu from anywhere on the page
